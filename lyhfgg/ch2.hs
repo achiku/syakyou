701be55a6f328b7c7aa9de69bcf17b899d5dc8be
Akira Chiku <akira.chiku@gmail.com>
2014-08-21 09:32:06 +0900

    2014/08/21

--- lyhfgg/ch2.hs
+++ lyhfgg/ch2.hs
@@ -6,3 +6,8 @@ removeNonUppercase' st = [ c | c <- st, elem c ['A'..'Z'] ]
 
 addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
+
+length' xs = sum [1 | _ <- xs]
+
+triangles = [(a, b, c) | a <- [1..10], b <- [1..10], c <- [1..10]]
+righttriangles = [(a, b, c) | a <- [1..10], b <- [1..10], c <- [1..10], a^2 + b^2 == c^2]
---
+++ lyhfgg/ch4.hs
@@ -0,0 +1,23 @@
+lucky :: (Integral a) => a -> String
+lucky 7 = "lucky number seven!"
+lucky n = "sorry.."
+
+sayMe :: (Integral a) => a -> String
+sayMe 1 = "One!"  
+sayMe 2 = "Two!"  
+sayMe 3 = "Three!"  
+sayMe 4 = "Four!"  
+sayMe 5 = "Five!"  
+sayMe x = "Not between 1 and 5"
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n-1)
+
+charName :: Char -> String  
+charName 'a' = "Albert"  
+charName 'b' = "Broseph"  
+charName 'c' = "Cecil"  
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors a b = (fst a + fst b, snd a + snd b)
