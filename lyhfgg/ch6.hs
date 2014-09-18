ebdac8cf95644f6e43c2a2890b40a205f2f1dfbc
Akira Chiku <akira.chiku@gmail.com>
2014-09-18 09:31:47 +0900

    2014/09/18

--- lyhfgg/ch6.hs
+++ lyhfgg/ch6.hs
@@ -39,3 +39,17 @@ zipWith' f (x:xs) (y:ys) = f x y :zipWith' f xs ys
 flip' :: (a -> b -> c) -> (b -> a -> c)
 flip' f = g
     where g x y = f y x
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+    | p x        = x : filter p xs
+    | otherwise  = filter p xs
+
+-- numLogChain :: Int
+-- numLogChain = length (filter (\xs -> length xs > 15) (map chain [1..100]))
