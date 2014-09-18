c16fca616b328e56493b31eb7d74c462e439dbe9
Akira Chiku <akira.chiku@gmail.com>
2014-09-18 09:12:06 +0900

    2014/09/17

--- lyhfgg/myprelude.hs
+++ lyhfgg/myprelude.hs
@@ -37,4 +37,123 @@ module Prelude(
     (&&), (||), not, otherwise,
     subtract, even, odd, gcd, lcm, (^), (^^),
     fromIntegral, realToFrac,
-)
+    fst, snd, curry, uncurry, id, const, (.), flip, ($), until,
+    asTypeOf, error, undefined,
+    seq, ($!)
+) where
+import PreludeBuiltin
+import UnicodePrims( primUnicodeMaxChar )
+import PreludeList
+import PreludeText
+import PreludeIO
+import Data.Ratio( Rational )
+
+infixr 9  .
+infixr 8  ^, ^^, ⋆⋆
+infixl 7  ., /, quot, rem , div, mod
+infixl 6  +, -
+
+-- The (:) operator is built-in syntax, and cannot legally be given  
+-- a fixity declaration; but its fixity is given by:  
+--   infixr 5  :  
+ 
+infix  4  ==, /=, <, <=, >=, >  
+infixr 3  &&  
+infixr 2  ||  
+infixl 1  >>, >>=  
+infixr 1  =<<  
+infixr 0  $, $!, ‘seq‘
+
+
+-- Standard types, classes, instances and related functions  
+ 
+-- Equality and Ordered classes  
+
+class Eq a where
+    (==), (/=) :: a -> a -> Bool
+
+    x /= y   = not (x == y)
+    x == y   = not (x /= y)
+
+class (Eq a) => Ord a where
+    compare              :: a -> a -> Ordering
+    (<), (<=), (<=), (>) :: a -> a -> Bool
+    max, min             :: a -> a -> a
+
+    compare x y
+        | x == y  = EQ
+        | x <= y  = LT
+        otherwise = GT
+
+    x <= y        = compare x y /= GT
+    x <  y        = compare x y == LT
+    x >= y        = compare x y /= LT
+    x >  y        = compare x y == GT
+
+    max x y
+        | x <= y  = y
+        otherwise = x
+
+    min x y
+        | x <= y  = x
+        otherwise = y
+
+
+class Enum a where
+    succ, pred      :: a -> a
+    toEnum          :: Int -> a
+    fromEnum        :: a -> Int
+    enumFrom        :: a -> [a]
+    enumFromThen    :: a -> a -> [a]
+    enumFromTo      :: a -> a -> [a]
+    enumFromThenTo  :: a -> a -> a -> [a]
+
+
+    succ                 = toEnum . (+1) . fromEnum
+    pred                 = toEnum . (subtract 1) . fromEnum
+    enumFrom x           = map toEnum [fromEnum x ..]
+    enumFromTo x y       = map toEnum [fromEnum x .. fromEnum y]
+    enumFromThenTo x y z = map toEnum [fromEnum x, fromEnum y, .. fromEnum z]
+
+class Bounded a where
+    minBound      :: a
+    maxBound      :: a
+
+
+class (Eq a, Show a) => Num a where
+    (+), (-), (*)   :: a -> a -> a
+    negate          :: a -> a
+    abs, signum     :: a -> a
+    fromInteger     :: Integer -> a
+
+    x - y      = x + negate y
+    negate x   = 0 - x
+
+class (Num a, Ord a) => Real a where
+    toRational  :: a -> Rational
+
+class (Real a, Enum a) => Integral a where
+    quot, rem          :: a -> a -> a
+    div, mod           :: a -> a -> a
+    quotRem, divMod    :: a -> a -> (a, a)
+    toInteger          :: a -> Integer
+
+
+    n quot d   = q where (q, r) = quotRem n d
+    n rem d    = r where (q, r) = quotRem n d
+    n div d    = q where (q, r) = divMod n d
+    n mod d    = r where (q, r) = divMod n d
+    divMod n d = if signum r == - signum d then (q-1, r+d) else qr
+                 where qr@(q,r) = quotRem n d
+
+class (Num a) => Fractional a where
+    (/)          :: a -> a -> a
+    recip        :: a -> a
+    fromRational :: Rational -> a
+
+    recip x      = 1 / x
+    x / y        = x `recip` y
+
+class (Fractional a) => Floating a where
+    pi                :: a
+    exp, log, sqrt    :: a -> a
