322878348ffd4ad1e1f5eb9809ccba00b666284a
Akira Chiku <akira.chiku@gmail.com>
2014-10-01 09:43:01 +0900

    2014/10/01

--- lyhfgg/ch6.hs
+++ lyhfgg/ch6.hs
@@ -82,3 +82,10 @@ sum' xs = foldl (\acc x -> acc + x) 0 xs
 
 sum'' :: (Num a) => [a] -> a
 sum'' = foldl (+) 0
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' y ys = foldl (\acc x -> if x == y then True else acc) False ys
+
+mapr' :: (a -> b) -> [a] -> [b]
+mapr' f xs = foldr (\x acc -> f x : acc) [] xs
+
