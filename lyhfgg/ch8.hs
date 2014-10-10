f0647f4a772758c3b3c529d5f0d20ddea42fa16c
Akira Chiku <akira.chiku@gmail.com>
2014-10-10 10:01:07 +0900

    2014/10/10

--- lyhfgg/ch8.hs
+++ lyhfgg/ch8.hs
@@ -13,3 +13,18 @@ nudge :: Shape -> Float -> Float -> Shape
 nudge (Circle (Point x y) r) a b = Circle (Point (x + a) (y + b)) r
 nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = 
     Rectangle (Point (x1 + a) (y1 + b)) (Point (x2 + a) (y2 + b))
+
+
+data Person = Person { firstName :: String
+                      ,lastName :: String
+                      ,age :: Int
+                      ,height :: Float
+                      ,phoneNumber :: String
+                      ,flavor :: String
+                    } deriving (Show)
+
+data Car = Car {company :: String, model :: String, year :: Int} deriving (Show)
+
+data Maybe a = Nothing | Just a
+
+data Car' a b c = Car' {company' :: a, model' :: b, year' :: c} deriving (Show)
