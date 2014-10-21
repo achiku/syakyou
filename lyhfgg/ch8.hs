789199065dd7fdf04b14d24cd0cc94c4c6c0bd94
Akira Chiku <akira.chiku@gmail.com>
2014-10-21 09:32:38 +0900

    20141021

--- lyhfgg/ch8.hs
+++ lyhfgg/ch8.hs
@@ -23,8 +23,37 @@ data Person = Person { firstName :: String
                       ,flavor :: String
                     } deriving (Show)
 
-data Car = Car {company :: String, model :: String, year :: Int} deriving (Show)
+-- data Car = Car {company :: String, model :: String, year :: Int} deriving (Show)
 
 data Maybe a = Nothing | Just a
 
-data Car' a b c = Car' {company' :: a, model' :: b, year' :: c} deriving (Show)
+data Car a b c = Car { company :: a  
+                     , model :: b  
+                     , year :: c   
+                     } deriving (Show) 
+
+data Vector a = Vector a a a deriving(Show)
+
+vplus :: (Num t) => Vector t -> Vector t -> Vector t
+(Vector i j k) `vplus` (Vector l m n) = Vector (i+l) (j+m) (k+n)
+
+vectMult :: (Num t) => Vector t -> t -> Vector t
+(Vector i j k) `vectMult` m = Vector (i*m) (j*m) (k*m)
+
+scalarMult :: (Num t) => Vector t -> Vector t -> t
+(Vector i j k) `scalarMult` (Vector l m n) = i*l + j*m + k*n
+
+
+
+type PhoneNumber = String
+type PhoneName = String
+type PhoneBook = [(PhoneName, PhoneNumber)]
+
+phoneBook =
+    [("betty","555-2938")
+    ,("bonnie","452-2928")
+    ,("patsy","493-2928")
+    ,("lucille","205-2928")
+    ,("wendy","939-8282")
+    ,("penny","853-2492")
+    ]
