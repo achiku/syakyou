79af3e916cf755416d88464f4c93a953088b843e
Akira Chiku <akira.chiku@gmail.com>
2014-10-23 10:00:16 +0900

    20141023

--- lyhfgg/ch8.hs
+++ lyhfgg/ch8.hs
@@ -28,7 +28,7 @@ data Person = Person { firstName :: String
 
 -- data Car = Car {company :: String, model :: String, year :: Int} deriving (Show)
 
-data Maybe a = Nothing | Just a
+-- data Maybe a = Nothing | Just a
 
 data Car a b c = Car { company :: a  
                      , model :: b  
@@ -74,7 +74,26 @@ type LockerMap = Map.Map Int (LockerState, Code)
 lockerLookup :: Int -> LockerMap -> Either String Code
 lockerLookup lockerNumber map =
     case Map.lookup lockerNumber map of
-        Nothing -> Left $ "Locker number " ++ lockerNumber ++ " doesn't exist."
+        Nothing -> Left $ "Locker number " ++ show lockerNumber ++ " doesn't exist."
         Just (state, code) -> if state /= Taken
             then Right code
             else Left $ "Locker " ++ show lockerNumber ++ " is already taken!"
+
+lockers :: LockerMap  
+lockers = Map.fromList   
+    [(100,(Taken,"ZD39I"))  
+    ,(101,(Free,"JAH3I"))  
+    ,(103,(Free,"IQSA9"))  
+    ,(105,(Free,"QOTSA"))  
+    ,(109,(Taken,"893JJ"))  
+    ,(110,(Taken,"99292"))  
+    ]
+
+-- data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)  
+infixr 5 :-:  
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord) 
+
+infixr 5 .++
+(.++) :: List a -> List a -> List a
+Empty .++ ys =  ys
+(x :-: xs) .++ ys = x :-: (xs .++ ys)  
