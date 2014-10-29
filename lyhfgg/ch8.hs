b1ecf08851ed6ab7b74b3f2a6599e0fb7f38c2cb
Akira Chiku <akira.chiku@gmail.com>
2014-10-29 09:33:32 +0900

    20141029

--- lyhfgg/ch8.hs
+++ lyhfgg/ch8.hs
@@ -100,7 +100,7 @@ Empty .++ ys =  ys
 
 data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving(Show, Read, Eq)
 
-singleton :: a -> Tree
+singleton :: a -> Tree a
 singleton x = Node x EmptyTree EmptyTree
 
 treeInsert :: (Ord a) => a -> Tree a -> Tree a
@@ -109,3 +109,23 @@ treeInsert x (Node a left right)
     | x == a = Node x left right
     | x < a = Node a (treeInsert x left) right
     | x > a = Node a left (treeInsert x right) 
+
+treeElem ::(Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)  
+    | x == a = True
+    | x < a = treeElem x left
+    | x > a = treeElem x right
+
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight where
+    Red == Red = True
+    Green == Green = True
+    Yellow == Yellow = True
+    _ == _ = False
+
+instance Show TrafficLight where
+    show Red = "Red light"
+    show Green = "Green light"
+    show Yellow = "Yellow light"
