4fb2b8739e9cd32fa22b70dbb3561d6fd6fc836c
Akira Chiku <akira.chiku@gmail.com>
2014-11-05 10:47:49 +0900

    20141105

--- lyhfgg/ch8.hs
+++ lyhfgg/ch8.hs
@@ -130,13 +130,37 @@ instance Show TrafficLight where
     show Green = "Green light"
     show Yellow = "Yellow light"
 
-class AEq a where
-    (==) :: a -> a -> Bool  
-    (/=) :: a -> a -> Bool  
-    x == y = not (x /= y)  
-    x /= y = not (x == y)   
+class YesNo a where
+    yesno :: a -> Bool
 
-instance (AEq m) => AEq (Maybe m) where
-    Just x == Just y = x == y
-    Nothing == Nothing = True
-    _ == _ = False
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+instance YesNo [a] where
+    yesno [] = False
+    yesno _ = True
+
+instance YesNo Bool where
+    yesno = id
+
+instance YesNo (Maybe a) where
+    yesno (Just _) = True
+    yesno Nothing = False
+
+instance YesNo (Tree a) where
+    yesno EmptyTree = False
+    yesno _ = True
+
+instance YesNo TrafficLight where
+    yesno Red = False
+    yesno _ = True
+
+instance Functor Tree where
+    fmap f EmptyTree = EmptyTree
+    fmap f (Node x leftsub rightsub) = Node (f x) (fmap f leftsub) (fmap f rightsub)
+
+instance Functor (Either a) where
+    fmap f (Right x) = Right (f x)
+    fmap f (Left x) = Left x
+
