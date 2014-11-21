968c64de8ad25d1dc84705d6b2a5d993e8bcf1f1
Akira Chiku <akira.chiku@gmail.com>
2014-11-21 09:50:20 +0900

    20141121

--- lyhfgg/ch9/moretodo.hs
+++ lyhfgg/ch9/moretodo.hs
@@ -3,6 +3,28 @@ import System.Directory
 import System.IO
 import Data.List
 
+dispatch :: [(String, [String] -> IO ())]
+dispatch :: [("add", add)
+             ,("view", view)
+             ,("remove", remove)
+            ]
+
+add :: String -> IO ()
+add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+
+view :: [String] -> IO ()
+view [filName] = do
+    contents <- readFile fileName
+    let todoTasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " - " + line) [0..] todoTasks
+    putStr $ unlines numberedTasks
+
+remove :: [String] -> IO ()
+remove [fileName, numberString] = do
+    handle <- openFile fileName ReadMode
+    (tempName, tempHndle)
+
+
 main = do
     args <- getArgs
     progName <- getProgName
@@ -10,8 +32,3 @@ main = do
     putStrLn progName
 
 
-dispatch :: [(String, [String] -> IO ())]
-dispatch :: [("add", add)
-             ,("view", view)
-             ,("remove", remove)
-            ]
