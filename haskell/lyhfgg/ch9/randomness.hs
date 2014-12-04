07b98ae3af112d588a665d7b5203d649b23b4699
Akira Chiku <akira.chiku@gmail.com>
2014-12-04 10:10:31 +0900

    20141204

--- haskell/lyhfgg/ch9/randomness.hs
+++ haskell/lyhfgg/ch9/randomness.hs
@@ -1,1 +1,12 @@
 import System.Random
+
+threeCoins :: StdGen -> (Bool, Bool, Bool)
+threeCoins gen =
+    let (firstCoin, newGen) = random gen
+        (secondCoin, newGen') = random newGen
+        (thirdCoin, newGen'') = random newGen'
+    in (firstCoin, secondCoin, thirdCoin) 
+
+main = do
+    let games = [threeCoins (mkStdGen x) | x <- [1..10]]
+    putStr $ unlines games
