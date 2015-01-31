a084c0baac6f5487f83b12beef007b0d82363dcb
Akira Chiku <akira.chiku@gmail.com>
2015-01-31 21:49:02 +0900

    add gorilla/mux to handle http request

--- golang/webapp/main.go
+++ golang/webapp/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gorilla/mux"
 	"github.com/russross/blackfriday"
 	"net/http"
 )
@@ -10,8 +12,53 @@ func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
 	rw.Write(markdown)
 }
 
+func HomeHandler(rw http.ResponseWriter, r *http.Request) {
+	http.FileServer(http.Dir("public"))
+}
+
+func PostIndexHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "post index")
+}
+
+func PostCreateHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "post create")
+}
+
+func PostEditHandler(rw http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	fmt.Fprintln(rw, "post edit:", id)
+}
+
+func PostShowHandler(rw http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	fmt.Fprintln(rw, "post show:", id)
+}
+
+func PostUpdateHandler(rw http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	fmt.Fprintln(rw, "post update:", id)
+}
+
+func PostDeleteHandler(rw http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	fmt.Fprintln(rw, "post delete:", id)
+}
+
 func main() {
-	http.HandleFunc("/markdown", GenerateMarkdown)
-	http.Handle("/", http.FileServer(http.Dir("public")))
+	r := mux.NewRouter().StrictSlash(false)
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/markdown", GenerateMarkdown)
+
+	// Posts collections
+	posts := r.Path("/posts").Subrouter()
+	posts.Methods("GET").HandlerFunc(PostIndexHandler)
+	posts.Methods("POST").HandlerFunc(PostCreateHandler)
+
+	// Posts singular
+	post := r.PathPrefix("/posts/{id}").Subrouter()
+	post.Methods("GET").Path("/edit").HandlerFunc(PostEditHandler)
+	post.Methods("GET").HandlerFunc(PostShowHandler)
+	post.Methods("PUT", "POST").HandlerFunc(PostUpdateHandler)
+	post.Methods("DELETE").HandlerFunc(PostUpdateHandler)
 	http.ListenAndServe(":8080", nil)
 }
