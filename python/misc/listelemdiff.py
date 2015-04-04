9ef5a70b3d29eac0332116c172638185f5420679
Akira Chiku <akira.chiku@gmail.com>
2015-04-04 20:25:08 +0900

    use set() to make list elements uniqu

--- python/misc/listelemdiff.py
+++ python/misc/listelemdiff.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-from functools import reduce
-
-
-def comp_elem(x, y):
-    return x == y
 
 
 def make_named_list(name, l):
@@ -14,11 +9,15 @@ if __name__ == '__main__':
     lists = [
         make_named_list('same_elems1', [1, 1, 1]),
         make_named_list('same_elems2', [1]),
+        make_named_list('same_elems3', ['test', 'test', 'test']),
         make_named_list('diff_elems1', [2, 1, 1]),
         make_named_list('diff_elems2', [1, 1, 2]),
         make_named_list('diff_elems3', [1, 2, 1]),
+        make_named_list('diff_elems4', ['test', 100, 2]),
     ]
 
+    print "## using normal function"
     for l in lists:
-        print "{}: {} -> {}".format(
-            l['name'], l['list'], reduce(comp_elem, l['list']))
+        num_distinct_elems = len(set(l['list']))
+        print "{}: {} -> NumDistinctElems: {}".format(
+            l['name'], l['list'], num_distinct_elems)
