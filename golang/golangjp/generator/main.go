7684877e1d3c0fdf8f0965dd4e6f90077ef8a227
Akira Chiku <akira.chiku@gmail.com>
2015-05-03 22:58:00 +0900

    add more code

--- golang/golangjp/generator/main.go
+++ golang/golangjp/generator/main.go
@@ -7,6 +7,21 @@ import (
 	"time"
 )
 
+func Boring2(msg string, quit chan bool) <-chan string {
+	c := make(chan string)
+	go func() {
+		for {
+			select {
+			case c <- fmt.Sprintf("%s", msg):
+			case <-quit:
+				return
+			}
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+	return c
+}
+
 func Boring(msg string) <-chan string {
 	c := make(chan string)
 	go func() {
@@ -28,6 +43,21 @@ func FanIn(input1, input2 <-chan string) <-chan string {
 	go func() {
 		for {
 			c <- <-input2
+		}
+	}()
+	return c
+}
+
+func FanIn2(input1, input2 <-chan string) <-chan string {
+	c := make(chan string)
+	go func() {
+		for {
+			select {
+			case s := <-input1:
+				c <- s
+			case s := <-input2:
+				c <- s
+			}
 		}
 	}()
 	return c
@@ -60,4 +90,31 @@ func main() {
 		fmt.Println(<-c2)
 	}
 	fmt.Println("You're boring: I'm leaving.")
+
+	c3 := FanIn2(Boring("achiku"), Boring("moqada"))
+	for i := 0; i < 5; i++ {
+		fmt.Println(<-c3)
+	}
+	fmt.Println("You're boring: I'm leaving.")
+
+	// quit channel
+	quit := make(chan bool)
+	ide := Boring2("boring ide", quit)
+	for i := rand.Intn(10); i >= 0; i-- {
+		fmt.Println(<-ide)
+	}
+	quit <- true
+	fmt.Println("Quit.")
+
+	c4 := Boring("8maki")
+	timeout := time.After(2 * time.Second)
+	for {
+		select {
+		case s := <-c4:
+			fmt.Println(s)
+		case <-timeout:
+			fmt.Println("You talk too much.")
+			return
+		}
+	}
 }
