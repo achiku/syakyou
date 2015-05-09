acea878d06bf86052c643bb86a2698f3151ca2bd
Akira Chiku <akira.chiku@gmail.com>
2015-05-09 16:08:44 +0900

    request to server

--- golang/httpclient/main.go
+++ golang/httpclient/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"time"
+)
 
 type Todo struct {
 	Name      string `json:"name"`
@@ -9,5 +14,25 @@ type Todo struct {
 type Todos []Todo
 
 func main() {
-	client := http.Client{}
+	client := http.Client{
+		Timeout: time.Duration(10) * time.Second,
+	}
+	req, err := http.NewRequest("GET", "http://localhost:8080/", nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	dumps, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(dumps)
 }
