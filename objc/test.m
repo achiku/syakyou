35d39e237fe0921f8e71d0edcd7fa5531fe1be71
Akira Chiku <akira.chiku@gmail.com>
2015-08-06 09:19:32 +0900

    add class

--- objc/test.m
+++ objc/test.m
@@ -1,7 +1,18 @@
-#import <stdio.h>
+#import <Foundation/Foundation.h>
+
+
+@interface Person: NSObject
+
+@property (readonly) NSString *nickname;
+@property (readonly) NSString *firstname;
+@property (readonly) NSString *lastname;
+@property int age;
+
+- (void) greeting;
+@end
 
 
 int main() {
-    printf("hello, world!");
+    printf("hello, world!\n");
     return 0;
 }
