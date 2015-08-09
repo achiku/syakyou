3471a21439b9093c0b9eb6bb3d943428f276955e
Akira Chiku <akira.chiku@gmail.com>
2015-08-09 14:59:46 +0900

    add more methods

--- objc/test.m
+++ objc/test.m
@@ -3,16 +3,39 @@
 
 @interface Person: NSObject
 
-@property (readonly) NSString *nickname;
-@property (readonly) NSString *firstname;
-@property (readonly) NSString *lastname;
+@property (assign) NSString *nickname;
+@property (assign) NSString *firstname;
+@property (assign) NSString *lastname;
 @property int age;
 
 - (void) greeting;
+
+@end
+
+@implementation Person
+- (id) init:(NSString *)nickname : (NSString *)firstname : (NSString *)lastname : (int) age {
+    self = [super init];
+    self.nickname = nickname;
+    self.firstname = firstname;
+    self.lastname = lastname;
+    self.age = age;
+    return self;
+}
+
+- (void) greeting {
+    NSLog(@"Hello! My nickname is %@!\n", self.nickname);
+}
+
+- (void) politeGreeting {
+    NSLog(@"Good evening. My name is %@, %@.\n", self.firstname, self.lastname);
+}
 @end
 
 
+
 int main() {
-    printf("hello, world!\n");
+    Person *p = [[Person alloc] init:@"moqada" :@"Masahiko" :@"Okada" :29];
+    [p greeting];
+    [p politeGreeting];
     return 0;
 }
