67f4a51f62d9531c47db7566f3a76036b977770f
Akira Chiku <akira.chiku@gmail.com>
2015-10-19 01:56:40 +0900

    Add random wait sleep

--- python/misc/async_await.py
+++ python/misc/async_await.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 # http://makina-corpus.com/blog/metier/2015/python-http-server-with-the-new-async-await-syntax
+import random
 import asyncio
 
+
 async def slow_operation(n):
-    await asyncio.sleep(1)
-    print("Slow operation {} complete".format(n))
+    wait_sec = random.randint(1, 3)
+    await asyncio.sleep(wait_sec)
+    print("Slow operation {} complete [waited {} sec]".format(n, wait_sec))
 
 async def main():
     await asyncio.wait([
