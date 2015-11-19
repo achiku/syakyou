33c70766233fa783ce4b170744cb407bad3b7966
Akira Chiku <akira.chiku@gmail.com>
2015-11-19 16:51:42 +0900

    Add random forest using party package

---
+++ r/conditional_imp/party.R
@@ -0,0 +1,10 @@
+library(party)
+set.seed(my.seed)
+
+pmodel <- cforest(LABEL ~ ., data=my.data, control=cforest_unbiased(mtry=5, ntree=51))
+imp <- varimp(pmodel, conditional=F)
+# imp <- varimp(pmodel, conditional=T)
+fweights.party <- data.table(Features=names(imp), RealWeight=c(w, 0, 0), ConditionalImp=imp)
+fweights.party <- fweights.party[order(-ConditionalImp)]
+fweights.party$ConditionalImp <- as.integer(fweights.party$ConditionalImp * 100000)
+fweights.party
