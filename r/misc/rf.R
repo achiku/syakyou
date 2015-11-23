300d159d0f7f09977b5625cd02cd4503914c9e7b
Akira Chiku <akira.chiku@gmail.com>
2015-11-23 16:09:15 +0900

    Add Random Forest predict example

---
+++ r/misc/rf.R
@@ -0,0 +1,14 @@
+library(randomForest)
+
+train <- seq.int(1, 150, by=2)
+test <- setdiff(1:150, train)
+
+Y <- iris[, colnames(iris) == "Species"]
+X <- iris[, colnames(iris) != "Species"]
+
+# tmpmodel <- tuneRF(X, Y, doBest=T)
+# tmpmodel -> mtry=4 looks good
+
+iris.rf <- randomForest(Species~., data=iris[train,], mtry=4)
+iris.pred <- predict(iris.rf, iris[test,])
+table(iris[test,5], iris.pred)
