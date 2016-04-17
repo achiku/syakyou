fd4ca05040dfc732d5e399b6ba7c02ab9fe95447
Akira Chiku <akira.chiku@gmail.com>
2016-04-17 23:07:15 +0900

    Add more types

--- python/misc/typehinting_test.py
+++ python/misc/typehinting_test.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# http://stackoverflow.com/questions/32557920/what-are-type-hints-in-python-3-5
+# https://github.com/python/typeshed/
 
 
 def greeting(name: str) -> str:
@@ -6,7 +8,15 @@ def greeting(name: str) -> str:
     return 'Hello ' + name
 
 
+def add(x: int, y: int) -> int:
+    """add two numbers"""
+    return x + y
+
+
 if __name__ == '__main__':
     print(greeting('moqada'))
     greeting(11)
     greeting('achiku') + 1
+
+    add('a', 'b')
+    add(1, 3)
