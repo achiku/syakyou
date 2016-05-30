38e9c0643382f5ed880cb96276abb52fb5020695
Akira Chiku <akira.chiku@gmail.com>
2016-05-30 11:29:57 +0900

    Update

--- python/misc/typehinting_test.py
+++ python/misc/typehinting_test.py
@@ -1,6 +1,26 @@
 # -*- coding: utf-8 -*-
 # http://stackoverflow.com/questions/32557920/what-are-type-hints-in-python-3-5
 # https://github.com/python/typeshed/
+from typing import List
+
+
+class Person(object):
+    def __init__(self) -> None:
+        self.l = []  # type: List[str]
+
+    def add_name(self, name: str) -> None:
+        self.l.append(111)
+        self.l.append('aaa')
+        self.l.append(name)
+
+
+def helloall(names: List[str]) -> None:
+    for name in names:
+        print("hello, {}".format(name))
+
+
+def hello(name: str) -> None:
+    print("hello, {}".format(name))
 
 
 def greeting(name: str) -> str:
@@ -20,3 +40,7 @@ if __name__ == '__main__':
 
     add('a', 'b')
     add(1, 3)
+
+    hello('8maki')
+    hello(111)
+    helloall(['achiku', 'moqada', 'ideyuta'])
