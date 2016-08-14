a61252ecaf31c649e64b9d20e95ec51ca6a27786
Akira Chiku <akira.chiku@gmail.com>
2016-08-14 17:05:57 +0900

    Add Error

--- golang/misc/mappattern/main.go
+++ golang/misc/mappattern/main.go
@@ -2,8 +2,53 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 )
+
+// MyError my error
+type MyError struct {
+	orgError string
+	errList  []string
+}
+
+// NewMyError new error
+func NewMyError(s string) *MyError {
+	return &MyError{
+		orgError: s,
+		errList:  strings.Split(s, "|"),
+	}
+}
+
+// Error returns string
+func (m *MyError) Error() string {
+	return fmt.Sprintf("%s", m.errList)
+}
+
+// Has check if it has specified error
+func (m *MyError) Has(s string) bool {
+	for _, e := range m.errList {
+		if e == s {
+			return true
+		}
+	}
+	return false
+}
+
+// Equals check if it equals specific combination of error
+func (m *MyError) Equals(sl []string) bool {
+	if len(m.errList) != len(sl) {
+		return false
+	}
+	sort.Strings(m.errList)
+	sort.Strings(sl)
+	for i, v := range m.errList {
+		if sl[i] != v {
+			return false
+		}
+	}
+	return true
+}
 
 type errList []string
 
--- golang/misc/mappattern/main_test.go
+++ golang/misc/mappattern/main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestMap(t *testing.T) {
 	cases := map[string]string{
@@ -42,6 +45,29 @@ func TestSplit(t *testing.T) {
 		e := newErrList(c.s)
 		if e.has(c.key) != c.has {
 			t.Errorf("%v, %s", e, c.key)
+		}
+	}
+}
+
+func TestMyError(t *testing.T) {
+	cases := []struct {
+		s   string
+		key string
+		has bool
+	}{
+		{s: "", key: "E12345", has: false},
+		{s: "E12345", key: "E12345", has: true},
+		{s: "E12345", key: "E00000", has: false},
+		{s: "E12345|E122222|E193837", key: "E193837", has: true},
+		{s: "E12345|E122222|E193837", key: "E000000", has: false},
+	}
+	for _, c := range cases {
+		e := NewMyError(c.s)
+		if !e.Equals(strings.Split(c.s, "|")) {
+			t.Error("not equal")
+		}
+		if e.Has(c.key) != c.has {
+			t.Error("has failed")
 		}
 	}
 }
