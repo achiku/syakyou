fffabb2e6ea2e4f02ffbd984a7f0b5639476d742
Akira Chiku <akira.chiku@gmail.com>
2016-09-27 23:09:11 +0900

    Update

--- golang/misc/crypt/test.rb
+++ golang/misc/crypt/test.rb
@@ -6,3 +6,4 @@ body = "hoge=foo&bar=boo"
 message = nonce + url + body
 secret = "foo"
 p OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha256"), secret, message)
+# "9d6bbf1897701ec73bc80c395e98778756cb009164a949d62aaa6bb99e1223e1"
