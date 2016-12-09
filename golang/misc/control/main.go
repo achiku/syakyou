1740000ff07fb08b1f8e507bc010dac8b9ba5795
Akira Chiku <akira.chiku@gmail.com>
2016-12-09 10:44:13 +0900

    Update

--- golang/misc/control/main.go
+++ golang/misc/control/main.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("vim-go")
+	l := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	fmt.Print("Continue\n")
+	for i, n := range l {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Printf("%d: %d\n", i, n)
+	}
+
+	fmt.Print("Break\n")
+	for i, n := range l {
+		if n == 7 {
+			break
+		}
+		fmt.Printf("%d: %d\n", i, n)
+	}
 }
