29120ec94696efc69d175fb9bb8500f20e2845fd
Akira Chiku <akira.chiku@gmail.com>
2017-06-05 01:28:31 +0900

    Update

--- golang/misc/annonymous/main.go
+++ golang/misc/annonymous/main.go
@@ -7,17 +7,41 @@ type sts []struct {
 	Name string
 }
 
-type st struct {
+type stt struct {
 	ID   string
 	Name string
+}
+
+type st struct {
+	ID      string
+	Name    string
+	Address []struct {
+		State string
+		City  string
+	}
+}
+
+type address struct {
+	State string
+	City  string
 }
 
 func main() {
 	var ss sts
 	l := map[string]string{"achiku": "akira.chiku", "moqada": "masahiko.okada"}
 	for k, v := range l {
-		s := st{ID: k, Name: v}
+		s := stt{ID: k, Name: v}
 		ss = append(ss, s)
 	}
+	ad := []address{
+		address{State: "Texas", City: "Austin"},
+		address{State: "Texas", City: "Dallas"},
+	}
+	a := st{
+		ID:      "achiku",
+		Name:    "akira.chiku",
+		Address: ad,
+	}
 	fmt.Printf("%+v", ss)
+	fmt.Printf("%+v", a)
 }
