2cfc159d30be8e9ffbacc620b7ece8cdba308a47
Akira Chiku <akira.chiku@gmail.com>
2017-07-04 11:08:55 +0900

    Add

--- golang/misc/errorstest/main.go
+++ golang/misc/errorstest/main.go
@@ -1,21 +1,42 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"log"
 )
 
-func main() {
-	fmt.Printf("Hello, world\n")
-	fmt.Printf("%v\n", errors.New("aa"))
-	e := errors.New("")
-	if e == nil {
-		fmt.Printf("e is nil\n")
+type myErrorOne struct {
+	Msg string
+}
+
+func (e myErrorOne) Error() string {
+	return fmt.Sprintf("error one: msg=%s", e.Msg)
+}
+
+type myErrorTwo struct {
+	Msg string
+}
+
+func (e myErrorTwo) Error() string {
+	return fmt.Sprintf("error two: msg=%s", e.Msg)
+}
+
+func returnError(i int) error {
+	if i == 1 {
+		return myErrorOne{Msg: "fuck"}
+	} else if i == 2 {
+		return myErrorTwo{Msg: "shit"}
 	}
-	switch t := e.(type) {
-	default:
-		fmt.Printf("unexpected type %T", t)
-	case error:
-		fmt.Printf("error type %T", t)
+	return nil
+}
+
+func main() {
+	if err := returnError(1); err != nil {
+		switch err := err.(type) {
+		case myErrorOne:
+			log.Printf("my error one: %s", err.Error())
+		case myErrorTwo:
+			log.Printf("my error one: %s", err.Error())
+		}
 	}
 }
