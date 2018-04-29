f91b2b91f7694e1724e6f66ce5d8dfc6c26000e5
Akira Chiku <akira.chiku@gmail.com>
2018-04-29 18:26:18 +0900

    Refactor

--- golang/misc/scanner/main.go
+++ golang/misc/scanner/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "strings"
+
 var data = `100,data1,100data
 101,data2,101data
 102,data3,102data
@@ -12,16 +14,53 @@ var data = `100,data1,100data
 101,data10,101data
 `
 
+type parser struct {
+	f file
+}
+
+func newParser() *parser {
+	var f file
+	return &parser{
+		f: f,
+	}
+}
+
+func (p *parser) parse(l string) {
+	d := strings.Split(l, ",")
+	rec := record{
+		category:     d[0],
+		content:      d[1],
+		categoryData: d[2],
+	}
+	// if category ends with 0, it's a start of the new transaction
+	// others are component of the transaction
+	// category must be ascending order within the same transaction
+	typ := rec.categoryType()
+	switch typ {
+	case "0":
+		tran := &transaction{
+			rec1: &rec,
+		}
+		p.f = append(p.f, tran)
+	case "1":
+		t := p.f[len(p.f)-1]
+		t.rec2 = &rec
+	case "2":
+		t := p.f[len(p.f)-1]
+		t.rec3 = &rec
+	}
+}
+
 type transaction struct {
-	rec1 *record
-	rec2 *record
-	rec3 *record
+	rec1 *record `json:"omitempty,rec_1"`
+	rec2 *record `json:"omitempty,rec_2"`
+	rec3 *record `json:"omitempty,rec_3"`
 }
 
 type record struct {
-	category     string
-	content      string
-	categoryData string
+	category     string `json:"category"`
+	content      string `json:"content"`
+	categoryData string `json:"category_data"`
 }
 
 func (r *record) categoryType() string {
--- golang/misc/scanner/main_test.go
+++ golang/misc/scanner/main_test.go
@@ -23,44 +23,14 @@ func TestReadDataByCSV(t *testing.T) {
 func TestReadDataByScanner(t *testing.T) {
 	fh := strings.NewReader(data)
 	sc := bufio.NewScanner(fh)
-	lineNum := 1
-	var (
-		tran *transaction
-		f    file
-	)
+	p := newParser()
 	for sc.Scan() {
-		l := sc.Text()
-		d := strings.Split(l, ",")
-		rec := record{
-			category:     d[0],
-			content:      d[1],
-			categoryData: d[2],
-		}
-		// if it's still one transaction
-		// if category ends with 0, it's start of the new transaction
-		// others are component of the transaction
-		// category must be ascending order within the same transaction
-		typ := rec.categoryType()
-		switch typ {
-		case "0":
-			tran = &transaction{
-				rec1: &rec,
-			}
-			f = append(f, tran)
-		case "1":
-			tran.rec2 = &rec
-		case "2":
-			tran.rec3 = &rec
-		}
-		// add to rec2, rec3 of the transaction
-		fmt.Printf("%d: %+v\n", lineNum, tran)
-		lineNum++
+		p.parse(sc.Text())
 	}
 	if err := sc.Err(); err != nil {
 		t.Fatal(err)
 	}
-
-	for _, a := range f {
+	for _, a := range p.f {
 		t.Logf("%+v, %+v, %+v", a.rec1, a.rec2, a.rec3)
 	}
 }
