4e537fd69cd748150265b7137aebc71e80222664
Akira Chiku <akira.chiku@gmail.com>
2020-11-24 08:47:44 +0900

    Add react + typescript tutorial

---
+++ typescript/my-app/src/App.tsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+
+  return (
+    <div className="App">
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        increment
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        decrement
+      </button>
+    </div>
+  );
+}
+
+export default App;
