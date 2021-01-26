b6a945ebf5e027073a21944b8f43242733356cec
Akira Chiku <akira.chiku@gmail.com>
2021-01-26 11:19:48 +0900

    add ordinal encoding

--- python/pandas/labelencoding.py
+++ python/pandas/labelencoding.py
@@ -1,4 +1,4 @@
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OrdinalEncoder
 import pandas as pd
 
 
@@ -6,7 +6,8 @@ if __name__ == '__main__':
     df1 = pd.DataFrame({
         'data1': range(6),
         'key1': ['A', 'B', 'C', 'A', 'B', 'C'],
-        'key2': ['A', 'B', 'C', 'A', 'B', 'C']
+        'key2': ['A', 'B', 'C', 'A', 'B', 'C'],
+        'key3': ['B', 'C', 'A', 'B', 'B', 'C']
         })
     df2 = pd.DataFrame({
         'data2': range(3),
@@ -14,10 +15,17 @@ if __name__ == '__main__':
         'key2': ['A', 'E', 'B']
         })
 
+    # LabelEncoder
     le = LabelEncoder()
     le.fit(df1['key1'].unique())
     print(le.transform(df1['key1']))
 
-    df1['label'] = le.transform(df1['key1'])
+    df1['label1'] = le.transform(df1['key1'])
     df1.drop('key1', axis=1, inplace=True)
+    print(df1)
+
+    # OrdinalEncoder
+    oe = OrdinalEncoder()
+    df1['label2'] = oe.fit_transform(df1[['key2']])
+    df1.drop('key2', axis=1, inplace=True)
     print(df1)
