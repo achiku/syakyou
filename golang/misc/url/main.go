0cc01c6db781f2d0a17359a837c64f59d66764ee
Akira Chiku <akira.chiku@gmail.com>
2022-11-02 10:27:23 +0900

    Update

--- golang/misc/url/main.go
+++ golang/misc/url/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/url"
+)
 
 func main() {
-	fmt.Println("test")
+	u := "https://example.com?key1=val1#1&key2=val...2&url=https://akirachiku.com?mykey=val"
+	pu, err := url.Parse(u)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(pu)
+	fmt.Println(url.QueryEscape(pu.String()))
+
+	param := url.Values{}
+	param.Set("key1", "val1")
+	param.Set("key2", "val...2")
+	param.Set("url", "https://akirachiku.com?mykey=val")
+	fmt.Println(param.Encode())
 }
